f2463a6bf2d65ba133058074fec6e3ab785b7475
David Argumedo <dargumedo46@gmail.com>
2024-12-10 08:58:51 -0600

    Estados con React

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,13 +1,15 @@
+import React from 'react'
 import './TodoSearch.css'
 
 function TodoSearch(){
+
+    const [searchValue, setSearchValue /*estos nombres pueden llamarse de cualquier manera*/] = React.useState('')
+    console.log('los usuarios todos de ' + searchValue) //Aqui hace muchas impresiones porque lo hace cada vez que se cambia el state(cada vez que escriben)
+
     return(
-        <input className='TodoSearch' placeholder="Agregar nuevo dato" onChange={(event) => {
-            console.log('Escribiste en el todoSearch')
-            console.log(event)
-            console.log(event.target)
-            console.log(event.target.value)
-        }}></input>
+        <input className='TodoSearch' placeholder="Agregar nuevo dato" value={searchValue} onChange={(event) => {
+            setSearchValue(event.target.value);
+        }} ></input>
 
     );
 }
