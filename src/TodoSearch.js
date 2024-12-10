6ff6b94005381d1f3a789d746ce12a878e21618b
David Argumedo <dargumedo46@gmail.com>
2024-12-10 08:37:12 -0600

    Eventos: OnClick y OnChange

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,7 +2,12 @@ import './TodoSearch.css'
 
 function TodoSearch(){
     return(
-        <input className='TodoSearch' placeholder="Agregar nuevo dato"></input>
+        <input className='TodoSearch' placeholder="Agregar nuevo dato" onChange={(event) => {
+            console.log('Escribiste en el todoSearch')
+            console.log(event)
+            console.log(event.target)
+            console.log(event.target.value)
+        }}></input>
 
     );
 }
