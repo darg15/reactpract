f07ffd83a321cf286544ae7f1632ce0c59728687
David Argumedo <dargumedo46@gmail.com>
2024-12-16 15:58:31 -0600

    Buscando ToDo's

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import React from 'react';
 
 const defaultTodos = [
   {text: 'Ajustando elementos a la pantalla', completed: true},
-  {text: 'Elemento2', completed: false},
+  {text: 'Cositas', completed: false},
   {text: 'Elemento3', completed: false},
   {text: 'Elemento4', completed: false},
   {text: 'Usar estados derivados', completed: true}
@@ -19,7 +19,14 @@ function App() {
 
   const[todos, setTodos] = React.useState(defaultTodos); //React.userState() setea por defecto algo
   const [searchValue, setSearchValue /*estos nombres pueden llamarse de cualquier manera*/] = React.useState('')
-  console.log('los usuarios todos de ' + searchValue) //Aqui hace muchas impresiones porque lo hace cada vez que se cambia el state(cada vez que escriben)
+  console.log('los usuarios todos de ' + searchValue) //Aqui hace muchas impresiones porque lo hace cada vez que se cam = bia el state(cada vez que escriben)
+
+  const searchedTodos = todos.filter(todo =>
+      {
+        const todoText = todo.text.toLocaleLowerCase()
+        const searchText = searchValue.toLocaleLowerCase()
+        
+        return todoText.includes(searchText)}); //Buscando todos dentro de la lista
 
   const completedTodos = todos.filter(todo => !!todo.completed).length // !! significa doble negacion, y sirve para trabajar con valores true y false
   const totalTodos = todos.length;
@@ -36,7 +43,7 @@ function App() {
         {/* <TodoItem  />
         <TodoItem />
         <TodoItem /> */}
-        {defaultTodos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem key={todo.text} text={todo.text} completed={todo.completed}/>
         ))}
       </TodoList>
