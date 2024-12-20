dfffd37ce5ab93ddaca4be75001f81e8707bdb5b
darg15 <dargumedo46mail.com>
2024-12-19 21:47:27 -0600

    Iconos en React: librerias y SVG

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,12 +1,16 @@
+import {CompleteIcon} from './CompleteIcon'
+import {DeleteIcon} from './DeleteIcon'
 import './TodoItem.css'
 
 function TodoItem(props) {
     return (
       <li className="TodoItem">
         {/*Las comillas invertidas para hacer que el estilo sea dinamico*/}
-        <span className={`Icon Icon-check ${props.completed && "Icon-check--active"}`} onClick = {props.onComplete}>V</span>
+        {/* <span className={`Icon Icon-check ${props.completed && "Icon-check--active"}`} onClick = {props.onComplete}>V</span> */}
+        <CompleteIcon/>
         <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>{props.text}</p>
-        <span className="Icon Icon-delete" onClick = {props.onDelete}>X</span>
+        <DeleteIcon/>
+        {/* <span className="Icon Icon-delete" onClick = {props.onDelete}>X</span> */}
       </li>
     );
   }
